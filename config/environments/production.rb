17dc867fab90bc85c72d69cf9bf0a129fbbe57d4
Fauzan Qadri <ojankill@gmail.com>
2014-03-11 12:05:07 +0700

    production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ TiProject::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.assets.precompile += %w( *.js )
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
 
